a7d1f8f1e5a379f1865ee874e74c79efe8bfc5a6
Henrique Nunes <henriquesilvanunes0015@gmail.com>
2025-10-09 23:39:37 -0300

    chore: added the private route component to ensure route privacy

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,29 @@ import { HashRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login/Login";
 import AdminDashboard from "./pages/Dashboards/AdminDashboard";
 import CollaboratorDashboard from "./pages/Dashboards/ColaboradorDashboard";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
     <HashRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/dashboard-admin" element={<AdminDashboard />} />
-        <Route path="/dashboard-colaborador" element={<CollaboratorDashboard />} />
+        <Route
+          path="/dashboard-admin"
+          element={
+            <PrivateRoute allowedRoles={["ADMIN"]}>
+              <AdminDashboard />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/dashboard-colaborador"
+          element={
+            <PrivateRoute allowedRoles={["COLLABORATOR"]}>
+              <CollaboratorDashboard />
+            </PrivateRoute>
+          }
+        />
       </Routes>
     </HashRouter>
   );
