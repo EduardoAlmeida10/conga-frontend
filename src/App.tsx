59c10b79686edadd2a0fd16a2e8e0e08242b3cae
Henrique Nunes <henriquesilvanunes0015@gmail.com>
2025-10-23 11:42:17 -0300

    feat: added dashboardlayout to the app to adjust the screen view

--- src/App.tsx
+++ src/App.tsx
@@ -3,28 +3,31 @@ import Login from "./pages/Login/Login";
 import AdminDashboard from "./pages/Dashboards/AdminDashboard";
 import CollaboratorDashboard from "./pages/Dashboards/ColaboradorDashboard";
 import PrivateRoute from "./components/PrivateRoute";
+import DashboardLayout from "./components/DashboardLayout";
 
 function App() {
   return (
     <HashRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route
-          path="/dashboard-admin"
-          element={
-            <PrivateRoute allowedRoles={["ADMIN"]}>
-              <AdminDashboard />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/dashboard-colaborador"
-          element={
-            <PrivateRoute allowedRoles={["COLLABORATOR"]}>
-              <CollaboratorDashboard />
-            </PrivateRoute>
-          }
-        />
+        <Route element={<DashboardLayout />}>
+          <Route
+            path="/dashboard-admin"
+            element={
+              <PrivateRoute allowedRoles={["ADMIN"]}>
+                <AdminDashboard />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/dashboard-colaborador"
+            element={
+              <PrivateRoute allowedRoles={["COLLABORATOR"]}>
+                <CollaboratorDashboard />
+              </PrivateRoute>
+            }
+          />
+        </Route>
       </Routes>
     </HashRouter>
   );
