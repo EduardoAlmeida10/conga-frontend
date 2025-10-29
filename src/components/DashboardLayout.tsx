9822c6f78e8f6606d99902639b1765c7b70aa090
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2025-10-29 02:11:48 -0300

    feat: Change of title and addition of the calendar icon

--- src/components/DashboardLayout.tsx
+++ src/components/DashboardLayout.tsx
@@ -1,16 +1,34 @@
-import { Outlet } from "react-router-dom";
-import Sidebar from "./Sidebar";
+import { Outlet, useLocation } from "react-router-dom";
+import Sidebar from "./SideBar/Sidebar";
 import PageHeader from "./PageHeader";
+import { useAuth } from "../context/AuthContext";
 
 export default function DashboardLayout() {
   const today = new Date().toLocaleDateString("pt-BR");
-  const userName = "Carlos";
-  const title = "Gestão de Despesas"
+  const { user } = useAuth();
+  const name = user?.name ?? user?.username ?? "Usuário";
+  const location = useLocation();
+
+  const getTitle = () => {
+    switch (location.pathname) {
+      case "/dashboard-admin":
+        return "Visão Geral";
+      case "/dashboard-colaborador":
+        return "Visão Geral";
+      case "/despesas":
+        return "Gestão de Despesas";
+      case "/usuarios":
+        return "Gestão de Usuários";
+      default:
+        return "Dashboard";
+    }
+  };
+
   return (
     <div className="flex h-screen overflow-hidden gap-8">
       <Sidebar />
       <main className="flex-1 overflow-y-auto p-5">
-        <PageHeader title={title} userName={userName} date={today} />
+        <PageHeader title={getTitle()} nameUser={name} date={today} />
         <Outlet />
       </main>
     </div>
