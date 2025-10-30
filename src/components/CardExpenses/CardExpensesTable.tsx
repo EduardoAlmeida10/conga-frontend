efc4cdd37981ded672f7b4480e2167fb914ad1e1
Henrique Nunes <henriquesilvanunes0015@gmail.com>
2025-10-30 14:27:03 -0300

    feat: The functionality to read column names and retrieve column actions from the expensetable file has been added.

--- src/components/CardExpenses/CardExpensesTable.tsx
+++ src/components/CardExpenses/CardExpensesTable.tsx
@@ -1,27 +1,35 @@
 import iconDelete from "../../assets/iconDelete.svg"
 import iconEdit from "../../assets/iconEdit.svg"
+import type { BaseExpense } from "../../hooks/useExpenseData";
+
+interface Column {
+  key: string;
+  header: string;
+  render?: (item: any) => React.ReactNode;
+}
 
 interface CardExpensesTableProps {
   data: any[];
+  columns: Column[];
+  onEdit: (expense: BaseExpense) => void;
+  onDelete: (expense: BaseExpense) => void;
 }
 
-export default function CardExpensesTable({ data }: CardExpensesTableProps) {
+export default function CardExpensesTable({ data, columns, onEdit, onDelete }: CardExpensesTableProps) {
   if (!data || data.length === 0) {
     return <p>Nenhuma despesa cadastrada.</p>;
   }
-
-  const headers = Object.keys(data[0]);
 
   return (
     <table className="w-[950px]">
       <thead>
         <tr>
-          {headers.map((header) => (
+          {columns.map((col) => (
             <th
-              key={header}
+              key={col.key}
               className="p-2 text-justify border-b border-gray-200 px-6"
             >
-              {header.charAt(0).toUpperCase() + header.slice(1)}
+              {col.header}
             </th>
           ))}
           <th className="text-justify border-b border-gray-200">Ações</th>
@@ -30,16 +38,16 @@ export default function CardExpensesTable({ data }: CardExpensesTableProps) {
       <tbody>
         {data.map((item, idx) => (
           <tr key={idx} className="border-t border-gray-200">
-            {headers.map((header) => (
-              <td key={header} className="p-2 text-justify px-6">
-                {item[header]}
+            {columns.map((col) => (
+              <td key={col.key} className="p-2 text-justify px-6">
+                {col.render ? col.render(item) : item[col.key]}
               </td>
             ))}
             <td className="p-2 flex justify-between">
-              <button className="cursor-pointer">
+              <button className="cursor-pointer" onClick={() => onEdit(item)}>
                 <img src={iconEdit} alt="edit" />
               </button>
-              <button className="cursor-pointer">
+              <button className="cursor-pointer" onClick={() => onDelete(item)}>
                 <img src={iconDelete} alt="delete" />
               </button>
             </td>
