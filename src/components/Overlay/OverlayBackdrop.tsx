e0211cbeffd28c12d1b4a443e4f08cd38ef44a14
Henrique Nunes <henriquesilvanunes0015@gmail.com>
2025-10-30 14:28:40 -0300

    feat: In the overlaybackdrop file, the onClose prop has been modified to be mandatory.

--- src/components/Overlay/OverlayBackdrop.tsx
+++ src/components/Overlay/OverlayBackdrop.tsx
@@ -1,14 +1,14 @@
 interface BackdropProps {
   children: React.ReactNode;
   isOpen: boolean;
-  onClose?: () => void;
+  onClose: () => void;
 }
 
-export default function Backdrop({isOpen, children }: BackdropProps) {
+export default function Backdrop({isOpen, children, onClose }: BackdropProps) {
   if (!isOpen) return null;
   return (
-    <div className="fixed inset-0 bg-black/40 flex justify-center items-center z-50">
-      <div>{children}</div>
+    <div className="fixed inset-0 bg-black/40 flex justify-center items-center z-50" onClick={onClose}>
+      <div onClick={(e) => e.stopPropagation()}>{children}</div>
     </div>
   );
 }
