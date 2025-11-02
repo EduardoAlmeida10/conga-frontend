0ee438b487caca3defd7f6778eaaed24ff168461
Henrique Nunes <henriquesilvanunes0015@gmail.com>
2025-11-02 14:36:14 -0300

    feat: added the utility cost hook

---
+++ src/hooks/useUtilityCosts.ts
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import { findAllUtilityCosts, type UtilityCost, type UtilityCostFilterDto } from "../api/utility-costApi";
+
+
+export function useUtilityCosts(filters: UtilityCostFilterDto) {
+  const [data, setData] = useState<UtilityCost[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+
+  const fetchData = async (currentFilters: UtilityCostFilterDto) => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await findAllUtilityCosts(currentFilters);
+      setData(response.data || []);
+    } catch (err) {
+      setError("Falha ao buscar despesas.");
+      console.error(err);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+
+  useEffect(() => {
+    if (!filters) return;
+    fetchData(filters);
+  }, [filters]);
+
+
+  return { data, isLoading, error, refetch: () => fetchData(filters) };
+}
