33d921d86f39e1b45239987dde0313af0a42be7e
Henrique Nunes <henriquesilvanunes0015@gmail.com>
2025-11-02 14:59:32 -0300

    feat: added the supplie cost hook

---
+++ src/hooks/useSupplieCosts.ts
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import {
+  findAllSupplieCosts,
+  type SupplieCost,
+  type SupplieCostFilterDto,
+} from "../api/supllie-costApi";
+
+
+export function useSupplieCosts(filters: SupplieCostFilterDto) {
+  const [data, setData] = useState<SupplieCost[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+
+  const fetchData = async (currentFilters: SupplieCostFilterDto) => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await findAllSupplieCosts(currentFilters);
+      console.log(response.data)
+      setData(response.data);
+    } catch (err) {
+      setError("Falha ao buscar despesas.");
+      console.error(err);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+
+  useEffect(() => {
+    if (!filters) return;
+    fetchData(filters);
+  }, [filters]);
+
+
+  return { data, isLoading, error, refetch: () => fetchData(filters) };
+}
