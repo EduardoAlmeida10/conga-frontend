daae626867842575001848947c2a553b1807d933
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2025-11-03 01:38:42 -0300

    feat: Added useUserSubmit hook for user registration.

---
+++ src/hooks/users/useUserSubmit.ts
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import {
+  registerUser,
+  type RegisterUserDto,
+  type User,
+} from "../../api/users-costApi";
+
+export function useUserSubmit() {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+  const [success, setSuccess] = useState<boolean>(false);
+  const [createdUser, setCreatedUser] = useState<User | null>(null);
+
+  const submitUser = async (userData: RegisterUserDto) => {
+    try {
+      setLoading(true);
+      setError(null);
+      setSuccess(false);
+
+      const response = await registerUser(userData);
+      setCreatedUser(response);
+      setSuccess(true);
+    } catch (err: any) {
+      setError(err.response?.data?.message || "Erro ao registrar usuário");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return {
+    submitUser,
+    loading,
+    error,
+    success,
+    createdUser,
+  };
+}
