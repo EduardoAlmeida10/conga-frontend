712d568a903d6d550e82200ac93e6d9c11d59ba1
TobiasTac <tobias.vida@live.com>
2025-11-14 06:43:29 -0300

    feat: collaborator sidebar option

--- src/components/DashboardLayout.tsx
+++ src/components/DashboardLayout.tsx
@@ -1,7 +1,8 @@
 import { Outlet, useLocation } from "react-router-dom";
-import Sidebar from "./SideBar/Sidebar";
-import PageHeader from "./PageHeader";
 import { useAuth } from "../context/AuthContext";
+import PageHeader from "./PageHeader";
+import Sidebar from "./SideBar/Sidebar";
+import SidebarCollaborator from "./SideBar/SidebarCollaborator";
 
 export default function DashboardLayout() {
   const today = new Date().toLocaleDateString("pt-BR");
@@ -9,12 +10,19 @@ export default function DashboardLayout() {
   const name = user?.name ?? user?.username ?? "Usuário";
   const location = useLocation();
 
+  const getSidebar = () => {
+    if (user?.role === "ADMIN") {
+      return <Sidebar />;
+    }
+    return <SidebarCollaborator />;
+  };
+
   const getTitle = () => {
     switch (location.pathname) {
       case "/dashboard-admin":
         return "Visão Geral";
       case "/dashboard-colaborador":
-        return "Visão Geral";
+        return "Registro de Produção Diária";
       case "/despesas":
         return "Gestão de Despesas";
       case "/usuarios":
@@ -26,7 +34,7 @@ export default function DashboardLayout() {
 
   return (
     <div className="flex h-screen overflow-hidden gap-20">
-      <Sidebar />
+      {getSidebar()}
       <main className="flex-1 overflow-y-auto p-5">
         <PageHeader title={getTitle()} nameUser={name} date={today} />
         <Outlet />
