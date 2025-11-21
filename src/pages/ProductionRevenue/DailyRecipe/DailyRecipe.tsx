7ed726db376bfa192e20c8c935c650e96d7e3f7e
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2025-11-21 02:33:35 -0300

    feat: creating files for the other screens

---
+++ src/pages/ProductionRevenue/DailyRecipe/DailyRecipe.tsx
@@ -0,0 +1,7 @@
+export default function DailyRecipe(){
+  return (
+    <div>
+      <h1>Tela de receita diária</h1>
+    </div>
+  )
+}
---
+++ src/pages/ProductionRevenue/LocalRecord/LocalRecord.tsx
@@ -0,0 +1,7 @@
+export default function LocalRecord(){
+  return (
+    <div>
+      <h1>Tela de registro local</h1>
+    </div>
+  )
+}
