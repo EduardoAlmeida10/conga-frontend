d8fb0dfde4af6f64d4f278f7ebfccc96af72b3de
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2025-11-21 02:32:54 -0300

    feat: adding all screens to the main component.

--- src/pages/ProductionRevenue/ProductionRevenue.tsx
+++ src/pages/ProductionRevenue/ProductionRevenue.tsx
@@ -1,20 +1,30 @@
-import ConstTypeTabs from "@/components/ConstTypeTabs"
 import { useState } from "react";
 
-const ConstTypes = ["Receita diária", "Registros total", "Registros do dia"];
+import ConstTypeTabs from "@/components/ConstTypeTabs"
+import TotalRecord from "./TotalRecord/TotalRecord";
+import DailyRecipe from "./DailyRecipe/DailyRecipe";
+import DailyRecord from "./DailyRecord/DailyRecord";
+import LocalRecord from "./LocalRecord/LocalRecord";
+
+const ConstTypes = ["Receita diária", "Registro total", "Registros do dia", "Registro Local"];
 
 export default function ProductionRevenue(){
 
-  const [selectedType, setSelectedType] = useState("Pessoal");
+  const [selectedType, setSelectedType] = useState(ConstTypes[0]);
 
   const handleTypeChange = (type: string) => {
     setSelectedType(type);
-    console.log("Tipo selecionado:", type);
   };
 
   return(
     <div>
       <ConstTypeTabs tabs={ConstTypes} onSelect={handleTypeChange} />
+      <div>
+        {selectedType === "Receita diária" && <DailyRecipe />}
+        {selectedType === "Registro total" && <TotalRecord />}
+        {selectedType === "Registros do dia" && <DailyRecord />}
+        {selectedType === "Registro Local" && <LocalRecord />}
+      </div>
     </div>
   )
 }
