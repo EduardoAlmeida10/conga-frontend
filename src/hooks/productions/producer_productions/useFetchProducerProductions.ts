3c471659015d3f7b3ec8a9b11fc2b916f36e0e46
TobiasTac <tobias.vida@live.com>
2025-11-26 23:07:57 -0300

    feat: implements filters requests in fetch producer

--- src/hooks/productions/producer_productions/useFetchProducerProductions.ts
+++ src/hooks/productions/producer_productions/useFetchProducerProductions.ts
@@ -1,35 +1,46 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import type {
+  PaginatedProducerProductions,
+} from "@/api/productions/productionProducer.ts";
 import {
   findAllProducerProductions,
 } from "@/api/productions/productionProducer.ts";
-import type {
-  FilterProducerProductionDto,
-  PaginatedProducerProductions,
-} from "@/api/productions/productionProducer.ts";
+import { useCallback, useEffect, useMemo, useState } from "react";
 
-export function useFetchProducerProduction(initialFilters: FilterProducerProductionDto = {}) {
+export function useFetchProducerProduction(pageIndex: number, pageSize: number) {
   const [data, setData] = useState<PaginatedProducerProductions | null>(null);
+  const [totalItems, setTotalItems] = useState(0);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
-  const [filters, setFilters] = useState(initialFilters);
 
-  async function fetchData() {
+
+  const filters = useMemo(
+    () => ({
+      page: pageIndex + 1,
+      limit: pageSize,
+    }),
+    [pageIndex, pageSize],
+  );
+
+  const fetchData = useCallback(async () => {
     try {
       setLoading(true);
+
       const res = await findAllProducerProductions(filters);
+
       setData(res);
+      setTotalItems(res.total ?? 0)
     } catch (err: any) {
       setError(err.response?.data?.message || "Erro ao carregar produções");
     } finally {
       setLoading(false);
     }
-  }
+  }, [filters]);
 
   useEffect(() => {
     fetchData();
-  }, [filters]);
+  }, [fetchData]);
 
-  return { data, loading, error, setFilters, refetch: fetchData };
+  return { data, totalItems, loading, error, refetch: fetchData };
 }
--- src/hooks/productions/producer_productions_request/useFetchProducerProductionRequest.ts
+++ src/hooks/productions/producer_productions_request/useFetchProducerProductionRequest.ts
@@ -1,42 +1,47 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import type {
+  ProducerProductionRequest
+} from "@/api/productions/productionProducerRequest";
 import {
   findAllProducerProductionRequests,
 } from "@/api/productions/productionProducerRequest";
-import type {
-  FilterProducerProductionRequestDto,
-  PaginatedProducerProductionRequests,
-} from "@/api/productions/productionProducerRequest";
+import { useCallback, useEffect, useMemo, useState } from "react";
 
-export function useFetchProducerProductionRequests(filters?: FilterProducerProductionRequestDto) {
-  const [data, setData] = useState<PaginatedProducerProductionRequests>({
-    total: 0,
-    page: 1,
-    limit: 10,
-    totalPages: 0,
-    data: [],
-  });
+export function useFetchProducerProductionRequests(pageIndex: number, pageSize: number, status?: string) {
+  const [data, setData] = useState<ProducerProductionRequest[]>([]);
+  const [totalItems, setTotalItems] = useState(0);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
 
-  const fetchData = async (filterParams?: FilterProducerProductionRequestDto) => {
+  const filters = useMemo(
+    () => ({
+      page: pageIndex + 1,
+      limit: pageSize,
+      status: status
+    }),
+    [pageIndex, pageSize, status],
+  );
+
+  const fetchData = useCallback(async () => {
     try {
       setLoading(true);
       setError(null);
 
-      const response = await findAllProducerProductionRequests(filterParams || filters || {});
-      setData(response);
+      const response = await findAllProducerProductionRequests(filters);
+
+      setData(response.data);
+      setTotalItems(response.total ?? 0)
     } catch (err: any) {
       setError(err.message || "Erro ao buscar requests");
     } finally {
       setLoading(false);
     }
-  };
-
-  useEffect(() => {
-    fetchData(filters);
   }, [filters]);
 
-  return { data, loading, error, refetch: fetchData };
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
+  return { data, totalItems, loading, error, refetch: fetchData };
 }
