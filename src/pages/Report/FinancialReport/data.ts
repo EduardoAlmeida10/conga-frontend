9a22d223da7c29b01220f021e7eaba0c3117f1db
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2025-11-28 01:59:26 -0300

    feat: structured FinancialReport screen

---
+++ src/pages/Report/FinancialReport/data.ts
@@ -0,0 +1,17 @@
+export const relatorios = [
+  {
+    categoria: "Alimentacao",
+    tipo: "receita",
+    valor: 10,
+  },
+  {
+    categoria: "Alimentacao",
+    tipo: "receita",
+    valor: 40,
+  },
+  {
+    categoria: "Alimentacao",
+    tipo: "receita",
+    valor: 150,
+  },
+];
