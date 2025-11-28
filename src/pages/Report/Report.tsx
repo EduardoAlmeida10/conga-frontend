d58b648baf9770cba6d3eca8300c3e989a39bed0
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2025-11-27 21:08:31 -0300

    feat: structure of the main reports page

--- src/pages/Report/Report.tsx
+++ src/pages/Report/Report.tsx
@@ -1,7 +1,29 @@
-export default function Report(){
-  return(
-    <div>
-      <h1>PÁGINA DE RELATÓRIOS</h1>
+import ConstTypeTabs from "@/components/ConstTypeTabs";
+import { useState } from "react";
+import FinancialReport from "./FinancialReport/FinancialReport";
+import RevenueAnalysis from "./RevenueAnalysis/RevenueAnalysis";
+import ExpenseAnalysis from "./ExpenseAnalysis/ExpenseAnalysis";
+
+const ConstTypes = [
+  "Relatório Financeiro",
+  "Análise de Receita",
+  "Análise de Despesas",
+];
+
+export default function Report() {
+  const [selectedType, setSelectedType] = useState(ConstTypes[0]);
+
+  const handleTypeChange = (type: string) => {
+    setSelectedType(type);
+  };
+  return (
+    <div className="p-6 w-full max-w-6xl mx-auto">
+      <ConstTypeTabs tabs={ConstTypes} onSelect={handleTypeChange} />
+      <div>
+        {selectedType === "Relatório Financeiro" && <FinancialReport />}
+        {selectedType === "Análise de Receita" && <RevenueAnalysis />}
+        {selectedType === "Análise de Despesas" && <ExpenseAnalysis />}
+      </div>
     </div>
-  )
+  );
 }
