78ec1d368143deec99915652866b92e0c80c3b21
Henrique Nunes <henriquesilvanunes0015@gmail.com>
2025-11-28 08:25:07 -0300

    feat: Created a visual representation of the admin dashboard screen.

--- src/components/CardAnalysis.tsx
+++ src/components/CardAnalysis.tsx
@@ -4,12 +4,14 @@ interface CardAnalysisProps {
   title: string;
   value: number;
   children: ReactNode;
+  color?: string;
 }
 
 export default function CardAnalysis({
   title,
   value,
   children,
+  color = "bg-primary-100",
 }: CardAnalysisProps) {
   return (
     <div className="flex justify-center items-center py-4 w-60 gap-10 bg-white rounded-xl">
@@ -17,7 +19,7 @@ export default function CardAnalysis({
         <h1 className="text-gray-400 text-[16px]">{title}</h1>
         <p className="font-bold text-[17px]">R$ {value}</p>
       </label>
-      <div className="flex justify-center items-center h-12 w-12 bg-primary-100 rounded-xl text-white">
+      <div className={`flex justify-center items-center h-12 w-12 rounded-xl text-white ${color}`}>
         {children}
       </div>
     </div>
--- src/pages/AdminDashboard/AdminDashboard.tsx
+++ src/pages/AdminDashboard/AdminDashboard.tsx
@@ -1,43 +1,35 @@
-import OverviewSection from "@/components/Overview/OverviewSection";
-import type { CardItem } from "@/types/OverviewSection.types";
-import { AiOutlineAreaChart } from "react-icons/ai";
-import { FaDollarSign } from "react-icons/fa";
+import CardAnalysis from "@/components/CardAnalysis";
+import PendingApprovals from "@/components/PendingApprovals/PendingApprovals";
+import RecentRecords from "@/components/RecentRecord/RecentRecord";
+import { Activity, DollarSign, TrendingDown } from "lucide-react";
 
 export default function AdminDashboard() {
-  const dailyRecipeCards: CardItem[] = [
-    {
-      id: "dailyProduction",
-      icon: AiOutlineAreaChart,
-      title: "Produção Diária",
-      value: "430 LItros",
-      iconBgColor: "bg-blue-500",
-    },
-    {
-      id: "dailyReceive",
-      icon: AiOutlineAreaChart,
-      title: "Receita Diária",
-      value: "R$ 400,00",
-      iconBgColor: "bg-blue-500",
-    },
-    {
-      id: "mouthExpense",
-      icon: FaDollarSign,
-      title: "Despesas Mensal",
-      value: "R$ 4000,00",
-      iconBgColor: "bg-blue-500",
-    },
-    {
-      id: "monthReceive",
-      icon: FaDollarSign,
-      title: "Receita Mensal",
-      value: "R$ 5300,00",
-      iconBgColor: "bg-blue-500",
-    },
-  ];
-
   return (
-    <div>
-      <OverviewSection cards={dailyRecipeCards}></OverviewSection>
+    <div className="p-6 w-full text-white min-h-screen">
+      <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-4 gap-10">
+        <CardAnalysis title="Receita total" value={10} color="bg-blue-500">
+          <Activity className="bg-red" />
+        </CardAnalysis>
+        <CardAnalysis title="Receita total" value={10} color="bg-blue-500">
+          <TrendingDown className="bg-red" />
+        </CardAnalysis>
+        <CardAnalysis title="Receita total" value={10} color="bg-blue-500">
+          <DollarSign className="bg-red" />
+        </CardAnalysis>
+        <CardAnalysis title="Receita total" value={10} color="bg-blue-500">
+          <DollarSign className="bg-red" />
+        </CardAnalysis>
+      </div>
+
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 mt-8">
+
+        <div className="lg:col-span-2">
+          <RecentRecords />
+        </div>
+        <div className="lg:col-span-1">
+            <PendingApprovals/>
+        </div>
+      </div>
     </div>
   );
 }
