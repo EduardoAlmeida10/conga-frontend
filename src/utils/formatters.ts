0c77cbd3909e4070adbe3c7167116030039abdc5
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2025-12-01 22:21:00 -0300

    feat: presentation of formatted data

---
+++ src/utils/formatters.ts
@@ -0,0 +1,16 @@
+export function formatPeriod(dateFrom: string, dateTo: string) {
+  if (!dateFrom || !dateTo) return "";
+
+  const format = (d: string) => {
+    const safeDate = new Date(d.replace(/-/g, "/"));
+
+    return safeDate.toLocaleDateString("pt-BR", {
+      month: "short",
+      day: "2-digit",
+    })
+    .replace(".", "")
+    .replace(/^\w/, c => c.toUpperCase());
+  };
+
+  return `${format(dateFrom)} - ${format(dateTo)}`;
+}
