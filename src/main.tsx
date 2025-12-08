2e006903644f701622348b93bdb85033cd747639
TobiasTac <tobias.vida@live.com>
2025-12-08 03:03:58 -0300

    fix: QueryClient import

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
+import { QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App.tsx";
 import { AuthProvider } from "./context/AuthContext.tsx";
-import { QueryClientProvider } from "@tanstack/react-query";
-import { queryClient } from "./queryClient";
+import "./index.css";
+import { queryClient } from "./QueryClient.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
