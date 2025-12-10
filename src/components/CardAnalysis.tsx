a7abf374171056c86c1e9dd1bf62ff5dde890613
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2025-12-10 00:04:35 -0300

    chore: formation of values

--- src/components/CardAnalysis.tsx
+++ src/components/CardAnalysis.tsx
@@ -1,5 +1,5 @@
 import type { ReactNode } from "react";
-
+import { formatCurrency } from "@/lib/formatters";
 interface CardAnalysisProps {
   title: string;
   value: number;
@@ -19,7 +19,7 @@ export default function CardAnalysis({
     <div className="flex justify-center items-center py-4 w-60 gap-10 bg-white rounded-xl">
       <label>
         <h1 className="text-gray-400 text-[16px]">{title}</h1>
-        {is === "valor" && <p className="font-bold text-[17px]">R$ {value.toFixed(2)}</p>}
+        {is === "valor" && <p className="font-bold text-[17px]">{formatCurrency(value)}</p>}
         {is === "litros" && <p className="font-bold text-[17px]">{value} Litros</p>}
       </label>
       <div
