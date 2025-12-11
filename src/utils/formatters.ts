f3277c90c5b87288c2be632166507181f486cff7
TobiasTac <tobias.vida@live.com>
2025-12-11 15:05:52 -0300

    refactor: include year in format period

--- src/utils/formatters.ts
+++ src/utils/formatters.ts
@@ -7,9 +7,10 @@ export function formatPeriod(dateFrom: string, dateTo: string) {
     return safeDate.toLocaleDateString("pt-BR", {
       month: "short",
       day: "2-digit",
+      year: "numeric"
     })
-    .replace(".", "")
-    .replace(/^\w/, c => c.toUpperCase());
+      .replace(".", "")
+      .replace(/^\w/, c => c.toUpperCase());
   };
 
   return `${format(dateFrom)} - ${format(dateTo)}`;
